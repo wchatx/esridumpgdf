10d726c7a18382ec7356493041dcf3566803c6e3
wchatx <wchatx@gmail.com>
2020-09-13 11:26:46 -0500

    boilerplate

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='esridump-gdf',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/wchatx/esridump-gdf',
+    license='MIT',
+    author='Cole Howard',
+    author_email='wchatx@gmail.com',
+    description='ArcGIS Map and Feature Services to GeoDataFrame'
+)
