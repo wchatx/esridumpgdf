5115469e08fbb0798a4dd14ca0c3e4b735629e33
wchatx <wchatx@gmail.com>
2021-10-24 10:14:37 -0500

    stash utils

--- esridumpgdf/utils.py
+++ esridumpgdf/utils.py
@@ -2,6 +2,7 @@ from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import cpu_count
 from requests import Session
 import pandas as pd
+import geopandas as gpd
 
 
 def server_info(url) -> pd.DataFrame:
@@ -12,13 +13,8 @@ def server_info(url) -> pd.DataFrame:
     """
     session = Session()
     resp = session.get(url, params=dict(f='json')).json()
-
-    def _get_layers(service):
-        svc = session.get(service, params=dict(f='json')).json()
-        if 'layers' in svc:
-            for layer in svc['layers']:
-                
-        return svc
+    services = gpd.GeoDataFrame()
+    layers = gpd.GeoDataFrame()
 
     with ThreadPoolExecutor(max_workers=cpu_count()) as pool:
         services = pool.map(
@@ -26,9 +22,10 @@ def server_info(url) -> pd.DataFrame:
                             session.get(f'{url}/{folder}', params=dict(f='json')).json()['services']],
             resp['folders'])
         layers = pool.map(
-            _get_layers,
+            lambda service: print({k: v for k, v in session.get(service, params=dict(f='json')).json().items()}),
             [item for sublist in services for item in sublist]
         )
 
     server = pd.DataFrame.from_records([item for sublist in layers for item in sublist])
     return server
+
